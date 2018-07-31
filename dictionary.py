4b78e6ff19933aa66da0229329996e634555c1be
ShaneKoNaung <shanekonaung@gmail.com>
2018-07-31 23:32:34 +0630

    implemented word guess between line 14 and 21

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches as close
 
 dict = json.load(open('dictionary.json'))
 
@@ -10,7 +11,15 @@ def meaning(w):
     try:
         return dict[w]
     except KeyError:
-        return 'Invalid word'
+        close_words = close(w, dict.keys(), cutoff=0.75)
+        if len(close_words) > 0:
+            print("Did you mean {} instead? Enter Y if yes , N if no.".format(close_words[0]))
+            yn = input("Enter :")
+            if yn == 'Y':
+                return dict[close_words[0]]
+            else:
+                return "I don't understand your input."
+
 
 
 def dictionary():
